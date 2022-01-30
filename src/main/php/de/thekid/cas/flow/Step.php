8b6e23ba4fc5f4d149ecf393c66208f31c18d2f8
Timm Friebe <thekid@thekid.de>
2022-01-30 16:28:16 +0100

    QA: Document Step interface

--- src/main/php/de/thekid/cas/flow/Step.php
+++ src/main/php/de/thekid/cas/flow/Step.php
@@ -1,5 +1,17 @@
 <?php namespace de\thekid\cas\flow;
 
 interface Step {
-  
+
+  /**
+   * Sets up this step. Return a transmittable response, e.g. a View
+   * or a Redirect instance or NULL to proceed with the `complete()`
+   * function.
+   */
+  public function setup($req, $res, $session);
+
+  /**
+   * Complete this step. Returns NULL to complete this step and proceed
+   * with the next one, or an error to be shown.
+   */
+  public function complete($req, $res, $session);
 }
